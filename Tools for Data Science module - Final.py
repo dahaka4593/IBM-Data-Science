bb61b2aeb9751375e894814c73738dc3371259ad
Ahmed De Alba Garza <123248002+dahaka4593@users.noreply.github.com>
2023-07-14 15:40:04 -0600

    Add files via upload

---
+++ Tools for Data Science module - Final.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# ## In this notebook, Data Science Tools and Ecosystem are Summarized.
+
+# ## Objectives:
+# *  Learn how to use markdown
+# * Learn how to use tables
+# * Learn how to use arithmetic expressions in Python
+# 
+# ### Items covered:
+# * Popular Languages for Data Science
+# * Common libraries for data science in python
+# * Common open source IDEs for data science
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python 
+# 2. R
+# 3. SQL
+
+# ### Some of the commonly used libraries used by Data Scientists are:
+# 1. Numpy
+# 2. SciPy
+# 3. Pandas
+
+# | Data Science Tools |
+# |--------------------|
+# |        R           |
+# |      Jupyter       |
+# |  Apache Zeppelin   | 
+#  
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[8]:
+
+
+45*56+(4/3)
+# This expression multiplies 45 and 56 and adds 4/3 
+
+
+# In[9]:
+
+
+4 + 4
+#This expression adds two numbers
+
+
+# In[10]:
+
+
+25 / (5+10)
+#This expression divides 25 over the sum of 10+5.
+
+
+# In[11]:
+
+
+x = 200   #minutes
+y = x/60  #x in terms of hours. The decimal is not in minutes.
+print(y)
+
+
+# ## Author
+# Ahmed De Alba Garza
+# 
+
+# In[ ]:
+
+
+
+
